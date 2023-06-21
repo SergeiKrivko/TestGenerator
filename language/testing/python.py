ea7a4f9aa717baebb63fabb162274bd494b8e143
SergeiKrivko <sergik.krivko@gmail.com>
2023-06-21 20:42:06 +0300

    Починен coverage для Питона

--- language/testing/python.py
+++ language/testing/python.py
@@ -8,11 +8,10 @@ def python_compile(path, sm, cm, coverage):
 def python_run(path, sm, cm, args='', in_data='', coverage=False):
     command = f"\"{sm.get_smart('python_coverage', 'coverage')}\" run" \
         if coverage else f"\"{sm.get_smart('python', 'python')}\""
-    if os.path.isfile(path):
-        return cm.cmd_command(f"{command} {path} {args}", input=in_data, shell=True,
-                              timeout=float(sm.get_smart('time_limit', 3)))
-    return cm.cmd_command(f"{command} {path}/main.py {args}", input=in_data, shell=True,
-                          timeout=float(sm.get_smart('time_limit', 3)))
+    if not os.path.isfile(path):
+        path = f"{path}/main.py"
+    return cm.cmd_command(f"{command}{' -a' if coverage and os.path.isfile('.coverage') else ''} {path} {args}",
+                          input=in_data, shell=True, timeout=float(sm.get_smart('time_limit', 3)))
 
 
 def python_collect_coverage(path, sm, cm):
