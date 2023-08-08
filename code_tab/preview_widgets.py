63561fd93ee9d8d667ac0f18bb10eb9bae541350
SergeiKrivko <sergik.krivko@gmail.com>
2023-08-08 18:36:51 +0300

    Улучшен предпросмотр Html-файлов Добавлена возможность просмотра PDF-файлов

--- code_tab/preview_widgets.py
+++ code_tab/preview_widgets.py
@@ -1,5 +1,6 @@
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QUrl
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineSettings
 from PyQt5.QtWidgets import QTextEdit, QLabel, QWidget, QHBoxLayout
 
 
@@ -20,10 +21,16 @@ class PreviewWidget(QWidget):
         self.label.setAlignment(Qt.AlignCenter)
         layout.addWidget(self.label)
 
+        self.web_engine = QWebEngineView()
+        self.web_engine.settings().setAttribute(QWebEngineSettings.PluginsEnabled, True)
+        self.web_engine.settings().setAttribute(QWebEngineSettings.PdfViewerEnabled, True)
+        layout.addWidget(self.web_engine)
+
         self.setLayout(layout)
 
     def open(self, file: str):
         self.text_edit.hide()
+        self.web_engine.hide()
         self.label.hide()
 
         try:
@@ -31,10 +38,10 @@ class PreviewWidget(QWidget):
                 with open(file, encoding='utf-8') as f:
                     self.text_edit.setMarkdown(f.read())
                 self.text_edit.show()
-            if file.endswith('.html'):
-                with open(file, encoding='utf-8') as f:
-                    self.text_edit.setHtml(f.read())
-                self.text_edit.show()
+            if file.endswith('.html') or file.endswith('.pdf'):
+                file = file.replace('\\', '/')
+                self.web_engine.setUrl(QUrl(f"file:///{file}"))
+                self.web_engine.show()
             if file.endswith('.png') or file.endswith('.jpg') or file.endswith('.bmp'):
                 self.label.setPixmap(QPixmap(file))
                 self.label.show()
