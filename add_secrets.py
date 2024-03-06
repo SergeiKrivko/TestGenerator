d1869a22191afefe99fefdaf90c1934c913468b9
SergeiKrivko <sergik.krivko@gmail.com>
2024-03-06 12:43:16 +0300

    build

--- add_secrets.py
+++ add_secrets.py
@@ -37,7 +37,7 @@ def write_build_config():
     with open("src/config/build.py", 'w', encoding='utf-8') as f:
         for key in ['USE_WEB_ENGINE', 'USE_TELEGRAM']:
             f.write(f"{key} = {os.getenv(key, 'True').capitalize()}\n")
-        f.write(f"{key} = \"{os.getenv('BUILD_NAME', '').capitalize()}\"\n")
+        f.write(f"BUILD_NAME = \"{os.getenv('BUILD_NAME', '').capitalize()}\"\n")
     with open("src/config/build.py", encoding='utf-8') as f:
         print(f.read())
 
